9f6af0c2d968fd9f8dc2f703826e7674966ecb6a
Leana <leananeparidze.com>
2020-03-03 19:52:21 -0800

    adds gem icon to the rooms if there is treasure present

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import icon from '../assets/diamond.svg';
 
 function Room({room, selected, trigger}) {
 
   return (
-    <div style={{height: '15px', width: '100%',
-                 backgroundColor: `${room.room ? trigger[0] === room.x && trigger[1] === room.y ? "orange" : "green" : "transparent"}`,
+    <div style={{height: '30px', width: '100%',
+                 backgroundColor: `${room.room ? trigger[0] === room.x && trigger[1] === room.y ? "orange" : "lightgray" : "transparent"}`,
                  borderTop: `${room.up ? '1px solid transparent' : room.room ? '1px solid red' : '1px solid transparent'}`,
                  borderLeft: `${room.left ? '1px solid transparent' : room.room ? '1px solid red' : '1px solid transparent'}`,
                  borderRight: `${room.right ? '1px solid transparent' : room.room ? '1px solid red' : '1px solid transparent'}`,
-                 borderBottom: `${room.down ? '1px solid transparent' : room.room ? '1px solid transparent' : '1px solid transparent'}`
-    }}>   
+                 borderBottom: `${room.down ? '1px solid transparent' : room.room ? '1px solid transparent' : '1px solid transparent'}`,
+    }}>  
+     <div style={{position: 'absolute', display: `${room.room && room.roomData.items.length > 0 ? 'block' : 'none'}`}}>
+       <img src={icon} />
+     </div> 
     </div>
   )
 }
